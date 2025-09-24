15723d8fc2006e0b9decbcc2ca4a42f5af4417c2
nishant8272 <nishantverma01042005@gmail.com>
2025-09-25 01:11:25 +0530

    add regex for youtube

--- lib/util.ts
+++ lib/util.ts
@@ -1,1 +1,1 @@
-export const YT_REGEX = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:watch\?(?:.*&)?v=|embed\/|v\/))([A-Za-z0-9_-]{11})(?:[?&][^\s]*)?$/;
+export const YT_REGEX = /^(?:https?:\/\/)?(?:www\.|music\.)?(?:youtu\.be\/|youtube\.com\/(?:watch\?(?:.*&)?v=|embed\/|v\/|shorts\/))([A-Za-z0-9_-]{11})(?:[?&][^\s]*)?$/;
